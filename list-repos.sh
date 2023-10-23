cc836784d8173e9c52bf657176e81ccf9a64b3f8
Ivan Alejandro <ivanalejandro0@gmail.com>
2023-10-23 13:26:18 -0300

    Update helper script

--- list-repos.sh
+++ list-repos.sh
@@ -3,8 +3,9 @@
 # This script uses the json data we got from GitHub and use the `jq` app to
 # filter through the json information.
 
-cat repositories.json | jq -r -c '.[] | select(.isPrivate) | .name'  # show private repos, only names
-# cat repositories.json | jq -c '.[] | select(.isPrivate)'  # show private repos
-# cat repositories.json | jq -c '.[] | select(.isPrivate | not)'  # show public repos
-# cat repositories.json | jq -c '.[] | select(.isFork)'  # show forks
-# cat repositories.json | jq -c '.[] | select(.isFork | not)'  # show non forks
+jq -r '.[] | select(.isArchived) | .name' repositories.json  # show archived repos, only names
+# jq -r '.[] | select(.isPrivate) | .name' repositories.json  # show private repos, only names
+# jq -r '.[] | select(.isPrivate)' repositories.json  # show private repos
+# jq -r '.[] | select(.isPrivate | not)' repositories.json  # show public repos
+# jq -r '.[] | select(.isFork)' repositories.json  # show forks
+# jq -r '.[] | select(.isFork | not)' repositories.json  # show non forks
